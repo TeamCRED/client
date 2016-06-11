9eac934b24c0c522e3cb9fc955e8ae9e875c1fb9
CJ R <hello@cjr.co.de>
2016-06-11 10:15:14 -0600

    link to batch

--- public/javascripts/controllers/yourBrew.js
+++ public/javascripts/controllers/yourBrew.js
@@ -1,27 +1,46 @@
 app.controller('yourBrew', function($scope, $http, $stateParams){
   var server = 'http://localhost:3000/';
 
-  let beer_id = $stateParams.beer_id || '';
-  let quote = $stateParams.quote || '';
-  let date = $stateParams.date || '';
-  let time = $stateParams.time || '';
-  let tank = $stateParams.tank || '';
-
-  console.log($stateParams);
-  $scope.loading = true;
-
-  $http.get(server + 'beer/' + beer_id)
-    .then(function (result) {
-      $scope.beer = result.data;
-      return $http.get(server + 'batch?beer_id=' + beer_id + "&tank=" + tank + "&quote=" + quote + "&date=" + date + "&time=" + time);
-    }).then(function (result) {
+  if($stateParams.batch_id) {
+    $scope.loading = true;
+    $http.get(server + 'batch/' + $stateParams.batch_id)
+    .then(result => {
       if(result.data.batch) {
         $scope.batch = result.data.batch;
         $scope.employees = result.data.employees;
         $scope.users = result.data.users;
+        $http.get(server + 'beer/' + $scope.batch.beer_id)
+          .then(function (result) {
+            $scope.beer = result.data;
+          });
       } else {
         $scope.message = "Brew not found! :(";
       }
       $scope.loading = false;
-    })
+    });
+  } else {
+    let beer_id = $stateParams.beer_id || '';
+    let quote = $stateParams.quote || '';
+    let date = $stateParams.date || '';
+    let time = $stateParams.time || '';
+    let tank = $stateParams.tank || '';
+
+    console.log($stateParams);
+    $scope.loading = true;
+
+    $http.get(server + 'beer/' + beer_id)
+      .then(function (result) {
+        $scope.beer = result.data;
+        return $http.get(server + 'batch?beer_id=' + beer_id + "&tank=" + tank + "&quote=" + quote + "&date=" + date + "&time=" + time);
+      }).then(function (result) {
+        if(result.data.batch) {
+          $scope.batch = result.data.batch;
+          $scope.employees = result.data.employees;
+          $scope.users = result.data.users;
+        } else {
+          $scope.message = "Brew not found! :(";
+        }
+        $scope.loading = false;
+      })
+  }
 });
