382b62bb1e0480a68c5e19008c27d67f545ca587
CJ R <hello@cjr.co.de>
2016-06-11 06:11:14 -0600

    styles n such

--- public/javascripts/controllers/profile.js
+++ public/javascripts/controllers/profile.js
@@ -1,18 +1,23 @@
-app.controller('profile', function($scope, $http, Auth) {
+app.controller('profile', function($scope, $http, $state, Auth) {
   var award = 'http://localhost:3000/awards';
 
   $scope.user = Auth.getUser();
-  console.log($scope.user);
-  $scope.loading = true;
 
-  $http.get(award, {
-      headers: {
-        "Authorization": "Bearer " + localStorage.token,
-      }
-    })
-    .then(function(result) {
-      $scope.awards = result.data;
-      $scope.loading = false;
-      console.log($scope.awards);
-    });
+  if(!$scope.user) {
+    $state.go('login');
+  } else {
+    $scope.loading = true;
+
+    $http.get(award, {
+        headers: {
+          "Authorization": "Bearer " + localStorage.token,
+        }
+      })
+      .then(function(result) {
+        $scope.awards = result.data;
+        $scope.loading = false;
+        console.log($scope.awards);
+      });
+  }
+
 });
