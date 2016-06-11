7d63084dfa0958dd540f64737895155d55305889
Roberto Ortega <berto.ort@gmail.com>
2016-06-11 09:42:04 -0600

    tweass

--- public/javascripts/controllers/login.js
+++ public/javascripts/controllers/login.js
@@ -1,8 +1,9 @@
 app.controller('login', function($scope, Auth, $state){
+  $('.tooltipped').tooltip('remove');
   if(Auth.getUser()) {
     $state.go('dashboard.profile');
   }
-
+  $('.tooltipped').tooltip({delay: 50});
   $scope.login = function() {
     $scope.loading = true;
     console.log($scope.email, $scope.password);
