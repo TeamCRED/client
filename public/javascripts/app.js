fc5cfd1e6ed0c689304a49c7ba26cdc0e1c54fe5
Elana Kopelevich <ekopelevich@gmail.com>
2016-06-10 21:39:49 -0600

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,26 @@
+var app = angular.module('knowYourBrewApp', ['ui.router', 'ngAnimate']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+    .state('landing', {
+      url: '/',
+      templateUrl: 'partials/landing.html',
+    })
+    .state('dashboard', {
+      url: '/dashboard',
+      templateUrl: 'partials/dashboard.html'
+    })
+    .state('dashboard.profile', {
+      url: '/dashboard/profile',
+      templateUrl: 'partials/profile.html'
+    })
+    .state('dashboard.your-brew', {
+      url: '/dashboard/your-brew',
+      templateUrl: 'partials/profile.html'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'partials/login.html'
+    })
+  });
