1cd446340c14673e5a542c314503e4256191bfd6
CJ R <hello@cjr.co.de>
2016-06-11 09:05:20 -0600

    login user toast

--- public/javascripts/controllers/dashboard.js
+++ public/javascripts/controllers/dashboard.js
@@ -1,9 +1,7 @@
 app.controller('dashboard', function($scope, $http, Auth){
-  if(Auth.getUser()) {
-    Auth.getUser().then(function (user) {
-      $scope.user = user;
-    })
+  $scope.user = Auth.getUser();
+  if(!$scope.user) {
+    Materialize.toast('Login to join the game', 4000);
   }
-  Materialize.toast('Login to join the game', 4000)
   $('.tooltipped').tooltip({delay: 50});
 });
